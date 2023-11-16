7c4c8428095c8a9d759dc5cac827442bbbe8a647
mMustajabAhamd <mustajabajawah@gmail.com>
2023-11-16 22:16:09 +0500

    Database Configuration

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+const sequelize = require ('./config/sequelize.js') 
+
 require('dotenv').config()
 const port = process.env.port || 3001
 
