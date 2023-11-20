697f938ca0e089c64d703bd90ac62dfd9340a70f
omar-ahsan <106596821+omar-ahsan@users.noreply.github.com>
2023-11-21 00:52:30 +0500

    Created Attributes in announcement.model.js

---
+++ server/model/announcement.model.js
@@ -0,0 +1,28 @@
+const { sequelize, DataTypes } = require("./config/sequelize");
+
+const Announcement = sequelize.define("announcements", {
+    announcementID: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true
+    },
+    announcementTitle: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    announcementContent: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    announcementType: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+
+});
+
+sequelize.sync().then(() => {
+    console.log('Announcement table created successfully!');
+}).catch((error) => {
+    console.error('Unable to create table : ', error);
+});
