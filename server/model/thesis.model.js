b751a4c52676d0623d3882bf603f4a17accd1211
rania <raniabm21@gmail.com>
2023-11-23 14:57:01 +0500

    Created Synopsis and Thesis Models

---
+++ server/model/thesis.model.js
@@ -0,0 +1,39 @@
+const { sequelize, DataTypes } = require("../config/sequelize");
+
+const thesis = sequelize.define('thesis', {
+    thesisid: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    thesistitle: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    rollno: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    facultyid: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    internals:{
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        defaultValue: [],
+        allowNull: false
+    }
+});
+
+sequelize.sync().then(() => {
+    console.log('Thesis table created successfully!');
+}).catch((error) => {
+    console.error('Unable to create table : ', error);
+});
+
+// Export the Thesis model
+module.exports = { thesis };
