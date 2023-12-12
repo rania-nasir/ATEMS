4bc353e54001de141ec83a6a02d608be9bd20a14
omar-ahsan <omarkhattak0@gmail.com>
2023-12-12 23:10:16 +0500

    Improved readability for config

--- server/config/mailer.js
+++ server/config/mailer.js
@@ -1,7 +1,7 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer'); // using nodemailer
 
 // Create a transporter using SMTP or other transport options
-const transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({ // creating a transport with service and login of mail
     service: 'gmail',
     auth: {
         user: 'projectatems@gmail.com',
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport({
 });
 
 // Send mail function
-const sendMail = async (to, subject, text) => {
+const sendMail = async (to, subject, text) => { // sendMail function
     try {
         // Send mail with defined transport object
         const info = await transporter.sendMail({
