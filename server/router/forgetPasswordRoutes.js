0684e23d051bd9d4aaef084a9d0d74beaa98ebc9
mMustajabAhmad <mustajabajwah@gmail.com>
2024-02-14 00:32:28 +0500

    Forgot Password API for users

---
+++ server/router/forgetPasswordRoutes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const { sendMail } = require('../config/mailer');
+const { faculties } = require('../model/faculty.model');
+const { students } = require('../model/student.model');
+
+
+const forgetPassword = async (req, res) => {
+    const { email } = req.body; 
+
+    let user;
+    user = await faculties.findOne({ where: { email } });
+    if (!user) {
+        user = await students.findOne({ where: { email } });
+    }
+
+    if (!user) {
+        return res.status(404).json({ message: 'Email not registered in the system' });
+    }
+
+    
+    try {
+        
+        await sendMail(email, 'Password Recovery', `Your password is: ${user.password}`); // Send the password via email
+        return res.status(200).json({ message: 'Password sent successfully' });
+
+    } catch (error) {
+        console.error('Error sending email:', error);
+        return res.status(500).json({ message: 'Failed to send email' }); 
+    }
+};
+
+// Route for the forgot password functionality without authentication
+router.post('/forgetPassword', forgetPassword);
+
+module.exports = router;
