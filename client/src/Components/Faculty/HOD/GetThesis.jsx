cadc44a25a7b10e91d6e478ead59a41b2e1b9399
rania <raniabm21@gmail.com>
2024-02-21 00:09:06 +0500

    Implementation of HOD related thesis functionalities

---
+++ client/src/Components/Faculty/HOD/GetThesis.jsx
@@ -0,0 +1,103 @@
+import { useState, useEffect } from 'react';
+import Cookie from 'js-cookie';
+import { NavLink } from 'react-router-dom';
+
+export default function GetThesis() {
+    const [thesisData, setthesisData] = useState({ allThesis: [] });
+
+    useEffect(() => {
+        async function fetchthesisData() {
+            try {
+                const response = await fetch('http://localhost:5000/faculty/viewAllThesis', {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `${Cookie.get('jwtoken')}`
+                    }
+                });
+                if (response.ok) {
+                    const data = await response.json();
+                    setthesisData(data);
+
+                    // const rowData = data.map(item => item.thesisid);
+
+                    console.log('thesis Data -> ', data)
+
+                } else {
+                    throw new Error('Failed to fetch data');
+                }
+            } catch (error) {
+                console.error('Failed to retrieve data: ', error);
+            }
+        }
+
+        fetchthesisData();
+    }, []);
+
+    return (
+        <>
+            <div className='m-2'>
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-gray-950">
+                        Thesis Approval Requests
+                    </h2>
+                </div>
+                <div class="m-6 shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    Thesis ID
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Student Roll No.
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Supervisor ID
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Thesis Title
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Potential Areas
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Action
+                                </th>
+                            </tr>
+                        </thead>
+                        {thesisData.allThesis.length === 0 ? (
+                            <p className="px-6 py-4">No thesis found</p>
+                        ) : (
+                            <tbody>
+                                {thesisData.allThesis.map(rowData => (
+                                    <tr className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600" key={rowData.thesisid}>
+                                        <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {rowData.thesisid}
+                                        </td>
+                                        <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {rowData.rollno}
+                                        </td>
+                                        <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {rowData.facultyid}
+                                        </td>
+                                        <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {rowData.thesistitle}
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            {rowData.potentialareas}
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            <NavLink to={`/reviewThesis/${rowData.thesisid}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                                                View Details
+                                            </NavLink>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        )}
+                    </table>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ client/src/Components/Faculty/HOD/GetThesisDetails.jsx
@@ -0,0 +1,188 @@
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import Cookie from 'js-cookie';
+
+export default function GetThesisDetails() {
+
+    const navigate = useNavigate();
+    const { thesisid } = useParams();
+    const [ThesisData, setThesisData] = useState({ selectedThesis: null, facultyList: [] });
+
+    console.log('thesisid ==== ', thesisid);
+
+    useEffect(() => {
+        async function fetchThesisData() {
+            try {
+                const response = await fetch(`http://localhost:5000/faculty/reviewThesis/${thesisid}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `${Cookie.get('jwtoken')}`
+                    }
+                });
+                if (response.ok) {
+                    const data = await response.json();
+                    setThesisData(data);
+                    console.log('Thesis Data Detail + Faculty List --> ', data);
+                } else {
+                    throw new Error('Failed to fetch data');
+                }
+            } catch (error) {
+                console.error('Failed to retrieve data: ', error);
+            }
+        }
+
+        fetchThesisData();
+    }, [thesisid]);
+
+    const approveData = async (e) => {
+        e.preventDefault();
+
+        const res = await fetch(`http://localhost:5000/faculty/approveThesis/${thesisid}`, {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `${Cookie.get('jwtoken')}`
+            }
+        });
+
+        const data = await res.json();
+        console.log("Response data:", data); // Log the response data
+
+        if (res.status === 200) {
+            if (data.message === "Invalid Credentials") {
+                window.alert("Invalid Credentials");
+                console.log("Invalid Credentials");
+            } else {
+                window.alert("Accepted Thesis Successfully");
+                console.log("Accepted Thesis Successfully");
+                navigate('/');
+            }
+        } else {
+            window.alert("Something went wrong");
+            console.log("Something went wrong");
+        }
+    }
+
+    return (
+        <>
+         <div className='flex flex-1 flex-col justify-center items-center px-6 py-12 lg:px-8'>
+                <div className="mt-2 bg-gray-500 shadow overflow-hidden sm:rounded-lg w-[90%]">
+                    <div className="px-4 py-5 sm:px-6">
+                        <p className="max-w-2xl text-md text-white">
+                            Thesis Approval Request Details
+                        </p>
+                    </div>
+                    {ThesisData.selectedThesis && (
+                        <div className="border-t border-gray-200">
+                            <dl>
+                                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <div className="sm:col-span-1">
+                                        <dt className="text-sm font-medium text-gray-500">
+                                            Thesis ID:
+                                        </dt>
+                                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0">
+                                            {ThesisData.selectedThesis.thesisid}
+                                        </dd>
+                                    </div>
+                                    <div className="sm:col-span-1">
+                                        <dt className="text-sm font-medium text-gray-500">
+                                            Thesis Title:
+                                        </dt>
+                                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0">
+                                            {ThesisData.selectedThesis.thesistitle}
+                                        </dd>
+                                    </div>
+                                    <div className="sm:col-span-1">
+                                        <dt className="text-sm font-medium text-gray-500">
+                                            Roll Number
+                                        </dt>
+                                        <dd className="text-sm text-gray-900">
+                                            {ThesisData.selectedThesis.rollno}
+                                        </dd>
+                                    </div>
+                                </div>
+                                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <div className="sm:col-span-1">
+                                        <dt className="text-sm font-medium text-gray-500">
+                                            Supervisor ID:
+                                        </dt>
+                                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0">
+                                            {ThesisData.selectedThesis.facultyid}
+                                        </dd>
+                                    </div>
+                                    <div className="sm:col-span-1">
+                                        <dt className="text-sm font-medium text-gray-500">
+                                            Internal 1:
+                                        </dt>
+                                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0">
+                                            {/* {ThesisData.selectedThesis.thesistitle} */}
+                                        </dd>
+                                    </div>
+                                    <div className="sm:col-span-1">
+                                        <dt className="text-sm font-medium text-gray-500">
+                                            Internal 2:
+                                        </dt>
+                                        <dd className="text-sm text-gray-900">
+                                            {/* {ThesisData.selectedThesis.rollno} */}
+                                        </dd>
+                                    </div>
+                                </div>
+                                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <div className="sm:col-span-1">
+                                        <dt className="text-sm font-medium text-gray-500">
+                                            Thesis Status
+                                        </dt>
+                                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0">
+                                            {ThesisData.selectedThesis.gcapproval}
+                                        </dd>
+                                    </div>
+                                    <div className="sm:col-span-1">
+                                        <dt className="text-sm font-medium text-gray-500">
+                                            Potential Areas
+                                        </dt>
+                                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0">
+                                            {ThesisData.selectedThesis.potentialareas}
+                                        </dd>
+                                    </div>
+                                    {/* Display file URL */}
+                                    <div className="sm:col-span-1">
+                                        <dt className="text-sm font-medium text-gray-500">
+                                            Proposal File
+                                        </dt>
+                                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0">
+                                            <a href={`http://localhost:5000${ThesisData.selectedThesis.fileURL}`} target="_blank" rel="noopener noreferrer" type='application/pdf'>
+                                                View Proposal
+                                            </a>
+                                        </dd>
+                                    </div>
+                                </div>
+                            </dl>
+                        </div>
+                    )}
+                </div>
+
+                {/* Approval/Decline buttons */}
+                <div className='mt-4 w-[90%] flex justify-center'>
+                    <form className="w-full">
+                        {/* Approval/Decline buttons */}
+                        <div className="mt-2 w-full flex flex-row gap-3">
+                            <div className="flex w-full justify-start">
+                                <button className="w-[20%] flex-shrink-0 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-md shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-3 py-2.5 text-center me-2 mb-2"
+                                    type="button"
+                                    name='approvedata'
+                                    onClick={approveData}
+                                >
+                                    APPROVE
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </>
+    );
+}
+
+
