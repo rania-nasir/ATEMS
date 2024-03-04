4dc83e4ff6da99da185875441fdc7a75343317bc
omar-ahsan <omarkhattak0@gmail.com>
2024-03-05 02:02:00 +0500

    Added Model for Mid Evaluation

---
+++ server/model/midEvaluation.model.js
@@ -0,0 +1,88 @@
+const { sequelize, DataTypes } = require("../config/sequelize");
+
+const midevaluations = sequelize.define('midevaluations', {
+    rollno: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    thesistitle: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    thesisid: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    facultyid: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    midEvaluationAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    },
+    literatureReviewRank: {
+        type: DataTypes.ENUM,
+        values: ['a', 'b', 'c', 'd'],
+        allowNull: false
+    },
+    paper1: {
+        type: DataTypes.STRING,
+        allowNull: true // Nullable because it's only required if literatureReviewRank is 'c'
+    },
+    paper2: {
+        type: DataTypes.STRING,
+        allowNull: true // Nullable because it's only required if literatureReviewRank is 'c'
+    },
+    problemGapIdentified: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    problemClearlyDefined: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    problemPlacement: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    solutionUnderstanding: {
+        type: DataTypes.ENUM,
+        values: ['a', 'b', 'c', 'd'],
+        allowNull: false
+    },
+    comments: {
+        type: DataTypes.TEXT('long'),
+        allowNull: false
+    },
+    internalname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    internalid: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    }
+});
+
+sequelize.sync().then(() => {
+    console.log('Mid Evaluation table created successfully!');
+}).catch((error) => {
+    console.error('Unable to create table : ', error);
+});
+
+// Export the Proposal Evaluaton model
+module.exports = { midevaluations };
